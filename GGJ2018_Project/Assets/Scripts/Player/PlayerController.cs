ea37eea8b6f6440373a88eead0bc690855339509
Jeremie <jeremie.debarry@gmail.com>
2018-01-27 02:39:01 +0100

    Player Controller Basic

---
+++ GGJ2018_Project/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	#region isMovingRight
+	private bool isMovingRight = false;
+
+	public bool GetIsMovingRight()
+	{
+		return isMovingRight;
+	}
+	#endregion
+	#region isMovingLeft
+	private bool isMovingLeft = false;
+
+	public bool GetIsMovingLeft()
+	{
+		return isMovingLeft;
+	}
+	#endregion
+	#region isJumping
+	private bool isKeyJump = false;
+
+	public bool GetIsJumping()
+	{
+		return isKeyJump;
+	}
+	#endregion
+
+	#region Key
+	[Header("Keys")]
+	[SerializeField]
+	private KeyCode keyRight;
+	[SerializeField]
+	private KeyCode keyLeft;
+	[SerializeField]
+	private KeyCode keyJump;
+	#endregion
+
+	#region Jump
+	[Header("Jump")]
+	[SerializeField]
+	private float jumpHeight;
+	[SerializeField]
+	private float jumpTime;
+	#endregion
+
+	[Header("Variables")]
+	[SerializeField]
+	private List<Transform> feets;
+	[SerializeField]
+	private float speed;
+	[SerializeField]
+	private float defaulGravity = 9.81f;
+
+	private Transform myTransform;
+	private Rigidbody2D myRigidBody;
+	private float gravity;
+
+	private void Start()
+	{
+		myTransform = transform;
+		myRigidBody = gameObject.GetComponent<Rigidbody2D>();
+		gravity = defaulGravity;
+	}
+
+	private void Update()
+	{
+		KeyUpdate();
+		Gravity();
+		Move();
+		Jump();
+	}
+
+	public void KeyUpdate()
+	{
+		if (Input.GetKey(keyRight))
+			isMovingRight = true;
+		else
+			isMovingRight = false;
+
+		if (Input.GetKey(keyLeft))
+			isMovingLeft = true;
+		else
+			isMovingLeft = false;
+
+		if (Input.GetKey(keyJump))
+			isKeyJump = true;
+		else
+			isKeyJump = false;
+	}
+
+	public void Gravity()
+	{
+		Vector3 moveDirection = myRigidBody.velocity;
+
+		moveDirection.y -= gravity * Time.deltaTime;
+		myRigidBody.velocity = moveDirection;
+	}
+
+	public void Move()
+	{
+		if (isMovingRight)
+		{
+			Vector3 moveDirection = myRigidBody.velocity;
+
+			moveDirection.x = 1.0f * speed * 100f * Time.deltaTime;
+
+			myRigidBody.velocity = moveDirection;
+			return;
+		}
+		else if (isMovingLeft)
+		{
+			Vector3 moveDirection = myRigidBody.velocity;
+
+			moveDirection.x = -1.0f * speed * 100f * Time.deltaTime;
+
+			myRigidBody.velocity = moveDirection;
+			return;
+		}
+		else
+		{
+			myRigidBody.velocity = new Vector2(0.0f, myRigidBody.velocity.y);
+		}
+	}
+
+	public void Jump()
+	{
+		if (IsOnGround())
+		{
+			if (isKeyJump)
+			{
+				Vector3 moveDirection = myRigidBody.velocity;
+
+				moveDirection.y = JumpForce(jumpHeight, jumpTime);
+				myRigidBody.velocity = moveDirection;
+			}
+		}
+	}
+
+	public float JumpForce(float height, float time)
+	{
+		gravity = CalculeGravity(height, time);
+		return (4.0f * height) / time;
+	}
+
+	public bool IsOnGround()
+	{
+		foreach (var trans in feets)
+		{
+			RaycastHit2D[] ray = Physics2D.RaycastAll(trans.position, -Vector2.up, 0.1f);
+
+			foreach (RaycastHit2D r in ray)
+			{
+				if (r.transform.tag == "Platform")
+				{
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private float CalculeGravity(float height, float time)
+	{
+		return (8.0f * height) / (time * time);
+	}
+}
