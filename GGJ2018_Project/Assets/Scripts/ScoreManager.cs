0f6005b63efa9fb303940660c09ee1addce4bdd1
Guillaume PARINGAUX <gparingaux@gmail.com>
2018-01-27 08:35:22 +0100

    + score manager + HUD manager

---
+++ GGJ2018_Project/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+	[SerializeField]
+	private int score = 0;
+
+	[SerializeField]
+	private float timer = 0.0f;
+	[SerializeField]
+	private int seconde = 0;
+
+	public void ResetScore()
+	{
+		score = 0;
+	}
+
+	public void AddScore(int add, int multiplicateur = 1)
+	{
+		score += (int)(add * multiplicateur);
+		OnScoreEvent(score);
+	}
+
+	public int GetScore()
+	{
+		return score;
+	}
+
+	private void Update()
+	{
+		++timer;
+		if (timer == 60)
+		{
+			timer = 0;
+			++seconde;
+		}
+	}
+
+	#region Events
+	#region OnScoreEvent
+	public delegate void ScoreEvent(int score);
+	private event ScoreEvent OnScoreEvent;
+
+	public void AddOnScoreEvent(ScoreEvent func)
+	{
+		OnScoreEvent += func;
+	}
+
+	public void RemoveOnScoreEvent(ScoreEvent func)
+	{
+		OnScoreEvent -= func;
+	}
+
+	private void ResetOnScoreEvent()
+	{
+		OnScoreEvent = null;
+	}
+
+	private void InvokeOnScoreEvent(int score)
+	{
+		if (OnScoreEvent != null)
+			OnScoreEvent(score);
+	}
+	#endregion
+	#endregion
+
+}
---
+++ GGJ2018_Project/Assets/Scripts/Ui/UiGameHud.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiGameHud : MonoBehaviour
+{
+	[SerializeField]
+	private ScoreManager scoreManager;
+
+	[SerializeField]
+	private Text txtScore;
+
+	private void OnEnable()
+	{
+		scoreManager.AddOnScoreEvent(UpdateScore);
+	}
+
+	private void OnDestroy()
+	{
+		scoreManager.RemoveOnScoreEvent(UpdateScore);
+	}
+
+	public void UpdateScore(int score)
+	{
+		txtScore.text = "Score : " + score.ToString();
+	}
+}
