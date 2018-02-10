3d6767ac8194ea2a4da70b96b54d345796661deb
jerem <toto@gmzil.com>
2018-02-10 15:38:52 +0100

    + Destruction des sons apres leur fin

---
+++ GGJ2018_Project/Assets/Scripts/DestroyAtEnd.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAtEnd : MonoBehaviour
+{
+	private AudioSource audioListener;
+
+	private void Start()
+	{
+		audioListener = gameObject.GetComponent<AudioSource>();
+	}
+
+	private void Update()
+	{
+		if (!audioListener.isPlaying)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
